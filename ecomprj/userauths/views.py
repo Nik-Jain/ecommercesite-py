7f75103b873bd382e8242b4f860aea123cb4f378
Nikhil Jain <Nikhil-Jain@hotmail.com>
2024-04-20 08:24:48 +0530

    Integrate simple signup template and authentication

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from userauths import forms as user_forms
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def register_view(request):
     
     if request.method == "POST":
         form = user_forms.UserRegisterForm(request.POST or None)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 
+                             message=f"Hello {username}, Your account is created successfully!")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("core:index")
 
     else:
         form=user_forms.UserRegisterForm()
