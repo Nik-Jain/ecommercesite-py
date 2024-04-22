b00cae8eed1a7fb5d8b475c84853cf8855a0e4ed
Nikhil Jain <Nikhil-Jain@hotmail.com>
2024-04-22 16:46:13 +0530

    Enable sign-in

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from userauths import forms as user_forms
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
     
@@ -23,3 +26,27 @@ def register_view(request):
     context = {'form':form}
 
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already Logged In!")
+        return redirect('core:index')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exists.")
+        
+        user = authenticate(request, email=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('core:index')
+        else:
+            messages.warning(request, f"User does not exisrts, create an account")
+    context = {
+
+    }
+    return render(request, 'userauths/sign-in.html', context)
